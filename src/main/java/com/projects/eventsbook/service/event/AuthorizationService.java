1beda090ad7c644ec0f3111f1039821b732f8d11
Kristian2109 <kristian.petrov1998@gmail.com>
2024-01-20 13:37:49 +0200

    Create authorization service for group members

--- src/main/java/com/projects/eventsbook/service/event/AuthorizationService.java
+++ src/main/java/com/projects/eventsbook/service/event/AuthorizationService.java
@@ -2,9 +2,14 @@ package com.projects.eventsbook.service.event;
 
 import com.projects.eventsbook.entity.Event;
 import com.projects.eventsbook.entity.EventGroup;
+import com.projects.eventsbook.entity.GroupMember;
 import com.projects.eventsbook.entity.User;
+import com.projects.eventsbook.enumerations.GroupRole;
 
 public interface AuthorizationService {
     public boolean canUserVisitEvent(Event eventToVisit, User user);
     public boolean canUserVisitGroup(EventGroup eventGroup, User user);
+    public void validateChangeGroupRole(GroupMember actor, GroupMember toBeChanged, GroupRole role);
+    public void validateManageMembershipRequest(GroupMember actor);
+
 }
--- src/main/java/com/projects/eventsbook/service/event/AuthorizationServiceImpl.java
+++ src/main/java/com/projects/eventsbook/service/event/AuthorizationServiceImpl.java
@@ -2,8 +2,15 @@ package com.projects.eventsbook.service.event;
 
 import com.projects.eventsbook.entity.Event;
 import com.projects.eventsbook.entity.EventGroup;
+import com.projects.eventsbook.entity.GroupMember;
 import com.projects.eventsbook.entity.User;
+import com.projects.eventsbook.enumerations.GroupRole;
+import com.projects.eventsbook.exceptions.InvalidOperationException;
+import org.springframework.stereotype.Service;
 
+import javax.swing.*;
+
+@Service
 public class AuthorizationServiceImpl implements AuthorizationService{
     @Override
     public boolean canUserVisitEvent(Event eventToVisit, User user) {
@@ -18,4 +25,27 @@ public class AuthorizationServiceImpl implements AuthorizationService{
                 .stream()
                 .noneMatch(groupMember -> groupMember.getUser().getId().equals(user.getId()));
     }
+
+    @Override
+    public void validateChangeGroupRole(GroupMember actor, GroupMember toBeChanged, GroupRole role)
+            throws InvalidOperationException {
+        GroupRole actorRole = actor.getGroupRole();
+        GroupRole toBeChangedRole = toBeChanged.getGroupRole();
+        if (toBeChangedRole.equals(GroupRole.OWNER)) {
+            throw new InvalidOperationException("Owner cannot be modified!");
+        }
+        if (!actorRole.equals(GroupRole.OWNER)) {
+            throw new InvalidOperationException("Only owner can modify roles!");
+        }
+    }
+
+    @Override
+    public void validateManageMembershipRequest(GroupMember actor) {
+        GroupRole actorGroupRole = actor.getGroupRole();
+        if (actorGroupRole.equals(GroupRole.VIEWER)) {
+            throw new InvalidOperationException("Viewer cannot decide a membership request!");
+        }
+    }
+
+
 }
