e1492b43d1c4b2d577118046198ec2aeece9ccfc
Kristian2109 <kristian.petrov1998@gmail.com>
2024-02-02 20:17:18 +0200

    Add transactional for group member service

--- src/main/java/com/projects/eventsbook/service/eventGroup/MemberService.java
+++ src/main/java/com/projects/eventsbook/service/eventGroup/MemberService.java
@@ -4,9 +4,12 @@ import com.projects.eventsbook.DTO.groupDomain.CreateMemberDTO;
 import com.projects.eventsbook.entity.EventGroup;
 import com.projects.eventsbook.entity.GroupMember;
 import com.projects.eventsbook.enumerations.GroupRole;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 public interface MemberService {
 
+    @Transactional(readOnly = true)
     GroupMember getMemberFromGroup(EventGroup eventGroup, Long userId);
     void invite(CreateMemberDTO createMemberDTO, Long actorId);
     void acceptInvitation(Long userId, Long groupId, Long actorId);
@@ -16,5 +19,6 @@ public interface MemberService {
     void acceptRequest(Long toBeAcceptedId, Long actorId, Long groupId);
     void removeMember(Long userId, Long groupId);
     void changeGroupMemberRole(Long groupId, Long toBeChangedId, Long actorId, GroupRole groupRole);
+    @Transactional(readOnly = true)
     public GroupMember getGroupMemberByGroupAndUser(Long userId, Long groupId);
 }
